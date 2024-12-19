645ba93a569343c94e34a9d02a9ffaa4fae97e3c
LansubTech2024 <lansubtechnologies2024@gmail.com>
2024-12-19 15:26:36 -0800

    added to some new changes

--- DB_connection/db_connection.js
+++ DB_connection/db_connection.js
@@ -2,23 +2,23 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 
-const sequelize = new Sequelize('opfact', 'myadminuser', 'Keerthi@05', {
-    host: 'backendserver.mysql.database.azure.com',
-    dialect: 'mysql',
-    port: 3306,                                         
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-   },
-    logging: false,
+// const sequelize = new Sequelize('opfact', 'myadminuser', 'Keerthi@05', {
+//     host: 'backendserver.mysql.database.azure.com',
+//     dialect: 'mysql',
+//     port: 3306,                                         
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//    },
+//     logging: false,
 
- });
+//  });
 
-// const sequelize = new Sequelize('opfact', 'root', 'Lansub@2024', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-// });
+const sequelize = new Sequelize('opfact', 'root', 'Lansub@2024', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
 
 async function testConnection() {
     try {
@@ -34,3 +34,26 @@ async function testConnection() {
 
 
 module.exports = sequelize;
+
+
+// const mongoose = require('mongoose');
+
+
+// const uri = 'mongodb://localhost:27017/mes_db'; 
+
+
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Database connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
+
+// connectToDatabase();
+
+// module.exports = mongoose;
